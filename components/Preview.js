675134b16a81fe1e012379785fa7a16ecc7ad377
Pascal Spiesz <pascal@slamcode.com>
2019-01-03 18:10:35 +0100

    feat: Add basic sanitization

--- components/Preview.js
+++ components/Preview.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import MDX from './MDX'
 
+// import custom components
 import { ResponsiveCalendar } from '@nivo/calendar'
+
+// import mast plugins
+import slugHeadings from 'remark-slug'
+import autoLinkHeadings from 'remark-autolink-headings'
+
+// import hast plugins
+import rehypePrism from '@mapbox/rehype-prism'
+import rehypeSanitize from 'rehype-sanitize'
 
 const scope = {
   Demo: props => <h1>This is a demo component</h1>,
@@ -16,13 +25,41 @@ class Preview extends React.Component {
   constructor(props) {
     super(props)
   }
-  /*   componentDidUpdate(prevProps, prevState) {
-    console.log(prevProps, prevState)
 
-  } */
   render() {
+    const sanitizerConfig = {
+      tagNames: [
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'h7',
+        'h8',
+        'ul',
+        'ol',
+        'li',
+        'br',
+        'b',
+        'i',
+        'strong',
+        'em',
+        'a',
+        'pre',
+        'code',
+        'img',
+        'demo',
+        'Demo',
+      ],
+    }
     return (
-      <MDX key="preview" scope={scope}>
+      <MDX
+        key="preview"
+        scope={scope}
+        mdPlugins={[slugHeadings, autoLinkHeadings]}
+        hastPlugins={[rehypePrism, [rehypeSanitize, sanitizerConfig]]}
+      >
         {this.props.content}
       </MDX>
     )
